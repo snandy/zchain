37d81662aa394446bfe48e36fabb6189fc3b862b
zhouyrt <zhouyrt@163.com>
2013-03-20 22:41:13 +0800

    ui html5-shim.js

---
+++ ui/html5-shim.js
@@ -0,0 +1,8 @@
+/**
+ * IE6-9 不支持placeholder属性
+ * https://developer.mozilla.org/zh-CN/docs/HTML/Element/Input
+ */
+$.html5shim = function() {
+	var el = this;
+	
+};
---
+++ ui/placeholder.js
@@ -0,0 +1,8 @@
+/**
+ * IE6-9 不支持placeholder属性
+ * https://developer.mozilla.org/zh-CN/docs/HTML/Element/Input
+ */
+$.fn.placeholder = function() {
+	var el = this;
+
+};
