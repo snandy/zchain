effdb4344cc264c584e4fd0925d7b069f1135462
zhouyrt <zhouyrt@163.com>
2013-05-04 16:23:17 +0800

    dom module

--- ui/html5-shim.js
+++
@@ -1,8 +0,0 @@
-/**
- * IE6-9 不支持placeholder属性
- * https://developer.mozilla.org/zh-CN/docs/HTML/Element/Input
- */
-$.html5shim = function() {
-	var el = this;
-	
-};
--- ui/placeholder.js
+++
@@ -1,8 +0,0 @@
-/**
- * IE6-9 不支持placeholder属性
- * https://developer.mozilla.org/zh-CN/docs/HTML/Element/Input
- */
-$.fn.placeholder = function() {
-	var el = this;
-
-};
