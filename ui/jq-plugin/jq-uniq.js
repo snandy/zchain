4c7a94660e31a296e0e0e772989cbf31684ca264
zhouyrt <zhouyrt@163.com>
2013-11-28 11:13:15 +0800

    $.fn.uniqClick

---
+++ ui/jq-plugin/jq-uniq.js
@@ -0,0 +1,19 @@
+~function($) {
+
+$.fn.uniqClick = function(handler, time) {
+    time = time || 1000
+    this.each(function(i, el) {
+        var canClick = true
+        $(el).click(function() {
+            if (canClick) {
+                handler.apply(this, arguments)
+                canClick = false    
+            }
+            setTimeout(function() {
+                canClick = true
+            }, time)
+        })
+    })
+}
+
+}(jQuery)
