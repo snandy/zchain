aeac51497c870696393149aa30aa3c715d30792c
zhouyrt <zhouyrt@163.com>
2014-05-02 12:10:07 +0800

    base.js

--- base/base.js
+++ base/base.js
@@ -266,3 +266,39 @@ function getAfterDay(str) {
     return result.join('-')
 }
 
+// 获取图片的最大尺寸， naturalWidth在FF，Chrome，Safari, Opera, IE9中已实现
+function hasDimensions(img) {
+  return !!((img.complete && typeof img.naturalWidth !== "undefined") || img.width);
+}
+// Get image's max-width:100%; in pixels
+function getMaxWidth(img) {
+  var maxWidth;
+
+  // Check if naturalWidth is supported
+  if (img.naturalWidth !== undefined) {
+    maxWidth = img.naturalWidth;
+
+  // Not supported, use in-memory solution as fallback
+  } else {
+    var image = new Image();
+    image.src = img.src;
+    maxWidth = image.width;
+  }
+
+  // Return the max-width
+  return maxWidth;
+}
+
+// 判断一个元素是否在视图窗口中
+// Determine if an element is in the visible viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect()
+  var html = document.documentElement
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth)
+  )
+}
+
