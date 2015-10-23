3bb8a58587778b06fd1c2f34a187d541bad5eac3
snandy <zhouyrt@gmail.com>
2015-10-23 18:15:36 +0800

    math.js

--- base/math.js
+++ base/math.js
@@ -1,4 +1,12 @@
 // 去除给定整数的余数
 function floor(num, integer) {
-	return num - num%integer
+	return num - num % integer
+}
+
+// toFixed 修复
+function toFixed(num, s) {
+	var times = Math.pow(10, s)
+	var des = num * times + 0.5
+	des = parseInt(des, 10) / times
+	return des + ''
 }
